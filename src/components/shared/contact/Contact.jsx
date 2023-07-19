843e62e21171f5f2e09fbbd4a85c208a319b2359
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-19 15:37:54 +0300

    feat: adds section contact

---
+++ src/components/shared/contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classNames/bind';
+import styles from './Contact.module.scss';
+
+const cx = classNames.bind(styles);
+
+import background from './images/background.jpg';
+
+const Contact = () => (
+  <section className={cx('contact')}>
+    <div className={cx('contact-container')}>
+      <img src={background} className={cx('background')} alt="" aria-hidden="true" loading="lazy" />
+      <p className={cx('text')}>Do you have any questions?</p>
+      <h2 className={cx('title')}>Contact us</h2>
+      <form className={cx('form')}>
+        <input type="text" name="mail" placeholder="Enter your mail" className={cx('form-mail')} />
+        <button type="submit" className={cx('form-button')}>
+          Send
+        </button>
+      </form>
+    </div>
+  </section>
+);
+
+export default Contact;
