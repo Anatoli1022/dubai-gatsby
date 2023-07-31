f5cdafcb4f79304bad9bad074db8df1e55791ba6
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-31 16:00:05 +0300

    feat: adds home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
+import Hero from 'components/pages/home/hero';
+import Apartment from 'components/pages/home/apartment';
+import Video from 'components/pages/home/video';
+import Information from 'components/pages/home/information';
+import Contact from 'components/shared/contact';
+import Articles from 'components/shared/articles';
+import Questions from 'components/pages/home/questions/questions';
 
-const HomePage = () => <Layout>Hello world!</Layout>;
+const HomePage = () => (
+  <Layout>
+    <Hero />
+    <Apartment />
+    <Video />
+    <Information />
+    <Contact />
+    <Articles />
+    <Questions />
+  </Layout>
+);
 
 export default HomePage;
 
