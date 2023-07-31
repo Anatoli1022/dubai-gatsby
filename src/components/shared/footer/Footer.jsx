62cb5e30cef9c437057d9138358a7f3d6d68cb53
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-31 15:46:42 +0300

    feat: adds section footer

---
+++ src/components/shared/footer/Footer.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import classNames from 'classNames/bind';
+import styles from './Footer.module.scss';
+
+const cx = classNames.bind(styles);
+
+import data from './footer-data';
+
+import facebook from '../../../images/facebook.svg';
+import twitter from '../../../images/twitter.svg';
+import youtube from '../../../images/youtube.svg';
+import instagram from '../../../images/instagram.svg';
+import linkedIn from '../../../images/in.svg';
+import logo from '../../../images/logo.svg';
+import { AccordionLink } from 'hooks/accordionLinks/accordionLinks';
+
+const Footer = () => (
+  <footer className={cx('footer')}>
+    <div className={cx('container', 'footer-container', 'container-large')}>
+      <img src={logo} alt="Dubai Realty" className={cx('logo')} loading="lazy" />
+      <nav className={cx('navigation')}>
+        {data.map((data, index) => {
+          return <AccordionLink key={index} data={data} />;
+        })}
+        <div className={cx('contact-container')}>
+          <h3 className={cx('title')}>Contact</h3>
+          <ul className={cx('contact-list')}>
+            <li className={cx('list-item')}>
+              <p className={cx('text-address')}>964 Worthington Drive Dubai, UAE</p>
+            </li>
+            <li className={cx('list-item')}>
+              <a href="mailto:dubairealty@mail.com" className={cx('mail')}>
+                dubairealty@mail.com
+              </a>
+            </li>
+          </ul>
+          <a href="" class={cx('button-link')}>
+            Book a consultation
+          </a>
+        </div>
+      </nav>
+      <div className={cx('footer-footer')}>
+        <a href="/" className={cx('copyright')}>
+          Copyright © 2022 Dubai Realty
+        </a>
+        <ul className={cx('list')}>
+          <li className={cx('item')}>
+            <a href="#" className={cx('item-link')}>
+              <img src={cx(facebook)} alt="facebook" />
+            </a>
+          </li>
+          <li className={cx('item')}>
+            <a href="#" className={cx('item-link')}>
+              <img src={twitter} alt="twitter" loading="lazy" />
+            </a>
+          </li>
+          <li className={cx('item')}>
+            <a href="#" className={cx('item-link')}>
+              <img src={youtube} alt="youtube" loading="lazy" />
+            </a>
+          </li>
+          <li className={cx('item')}>
+            <a href="#" className={cx('item-link')}>
+              <img src={instagram} alt="instagram" loading="lazy" />
+            </a>
+          </li>
+          <li className={cx('item')}>
+            <a href="#" className={cx('item-link')}>
+              <img src={linkedIn} alt="in" loading="lazy" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/shared/footer/footer-data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    title: 'Buy',
+    firstLink: 'Apartment in Dubai',
+    secondLink: 'House in Dubai',
+    thirdLink: 'Apartments in Dubai ',
+    fourthLInk: 'Loft in Dubai',
+    fifthLink: 'Penthouse in Dubai',
+    sixthLink: 'Villa in Dubai',
+  },
+  {
+    title: 'Services',
+    firstLink: 'Property management in Dubai, UAE',
+    secondLink: 'Sell ​​property in Dubai, UAE',
+    thirdLink: 'Rent property in Dubai, UAE',
+    fourthLInk: 'Investments in Dubai, UAE',
+    fifthLink: 'Real estate for cryptocurrency in Dubai',
+    sixthLink: 'Moving to Dubai, UAE',
+  },
+  {
+    title: 'Information',
+    firstLink: 'Video',
+    secondLink: 'Podcasts',
+    thirdLink: 'Laws',
+    fourthLInk: 'Questions and answers',
+    fifthLink: 'Books',
+    sixthLink: 'Articles',
+  },
+  {
+    title: 'About company',
+    firstLink: 'Jobs ',
+    secondLink: 'Story',
+    thirdLink: 'Licenses',
+    fourthLInk: 'Why are we',
+    fifthLink: 'Real estate agency',
+    sixthLink: 'Questions',
+  },
+];
+
+export default data;
