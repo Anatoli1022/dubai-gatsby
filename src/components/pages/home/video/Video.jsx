82e213df0c76b6c72dd55edf3156bb5cdcab684e
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-31 15:53:27 +0300

    fix: fix image in section video, and colors

--- src/components/pages/home/video/Video.jsx
+++ src/components/pages/home/video/Video.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import classNames from 'classNames/bind';
 import styles from './Video.module.scss';
 
-import video from './images/video.jpg';
 import play from './images/play.svg';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const cx = classNames.bind(styles);
 
 const Video = () => (
   <div className={cx('video')}>
-    <div className={cx('container', ' container_xl')}>
+    <div className={cx('container', ' container-large')}>
       <div className={cx('video-wrapper')}>
-        <img src={video} className={cx('background')} loading="lazy" alt="" aria-hidden="true" />
+        <StaticImage
+          src="./images/video.jpg"
+          className={cx('background')}
+          loading="lazy"
+          alt=""
+          aria-hidden="true"
+        />
         <button className={cx('button')}>
           <img src={play} alt="" aria-hidden="true" className={cx('image-play')} loading="lazy" />
         </button>
