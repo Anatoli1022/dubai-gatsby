6628dec2d9ad2914fdc0d9c46db15bec02055ef4
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-31 15:52:30 +0300

    feat: adds section questions in home page

---
+++ src/components/pages/home/questions/questions.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from 'classNames/bind';
+import { Accordion } from 'hooks/accordion/accordion';
+
+import { data } from './data';
+
+import styles from './questions.module.scss';
+
+const cx = classNames.bind(styles);
+
+const Questions = () => (
+  <section className={cx('questions')}>
+    <div className={cx('container', 'container-medium')}>
+      <h2 className={cx('title')}>faq</h2>
+      <ul className={cx('list')}>
+        {data.map((section, index) => (
+          <Accordion key={index} section={section} />
+        ))}
+      </ul>
+    </div>
+  </section>
+);
+
+export default Questions;
