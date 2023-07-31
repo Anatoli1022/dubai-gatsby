cc9cedf803f52749a4b05d5ca7602fad2e3632c8
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-31 21:08:24 +0300

    fix: fix section video , play video

--- src/components/pages/home/video/Video.jsx
+++ src/components/pages/home/video/Video.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classNames/bind';
 import styles from './Video.module.scss';
-
 import play from './images/play.svg';
 import { StaticImage } from 'gatsby-plugin-image';
+import YouTube from 'react-youtube';
 
 const cx = classNames.bind(styles);
 
-const Video = () => (
-  <div className={cx('video')}>
-    <div className={cx('container', ' container-large')}>
-      <div className={cx('video-wrapper')}>
-        <StaticImage
-          src="./images/video.jpg"
-          className={cx('background')}
-          loading="lazy"
-          alt=""
-          aria-hidden="true"
-        />
-        <button className={cx('button')}>
-          <img src={play} alt="" aria-hidden="true" className={cx('image-play')} loading="lazy" />
-        </button>
+const Video = () => {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+  const handlePlayVideo = () => {
+    setIsVideoPlaying(true);
+  };
+
+  return (
+    <div className={cx('video')}>
+      <div className={cx('container', 'container-large')}>
+        <div className={cx('video-wrapper')}>
+          {isVideoPlaying ? (
+            <YouTube videoId="dQw4w9WgXcQ" className={cx('youtube')} />
+          ) : (
+            <StaticImage
+              src="./images/video.jpg"
+              className={cx('background')}
+              loading="lazy"
+              alt=""
+              aria-hidden="true"
+            />
+          )}
+          {!isVideoPlaying && (
+            <button className={cx('button')} onClick={handlePlayVideo}>
+              <img
+                src={play}
+                alt=""
+                aria-hidden="true"
+                className={cx('image-play')}
+                loading="lazy"
+              />
+            </button>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Video;
