47006ece3c12fc5cb7a149839de96154a194a2b1
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-09-09 03:01:48 +0300

    feat: projects js files

---
+++ src/components/shared/projects/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    title: 'Villas',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat molestie integer aliquam consectetur. Faucibus vitae dui massa tellus magna sit.',
+    link: 'See project',
+    image: require('./images/projects-villas-2.jpg'),
+  },
+  {
+    title: 'Houses',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat molestie integer aliquam consectetur. Faucibus vitae dui massa tellus magna sit.',
+    link: 'See project',
+    image: require('./images/projects-houses.jpg'),
+  },
+  {
+    title: 'Villas',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat molestie integer aliquam consectetur. Faucibus vitae dui massa tellus magna sit.',
+    link: 'See project',
+    image: require('./images/projects-villas.jpg'),
+  },
+];
+
+export default data;
