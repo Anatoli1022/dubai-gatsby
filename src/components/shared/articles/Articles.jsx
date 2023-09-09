c4089f684df44a1256d4255a4164c34a562da173
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-09-09 03:02:24 +0300

    fix: fix articles styles and tags

--- src/components/shared/articles/Articles.jsx
+++ src/components/shared/articles/Articles.jsx
@@ -3,6 +3,7 @@ import classNames from 'classNames/bind';
 import styles from './Articles.module.scss';
 
 import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
 
 const cx = classNames.bind(styles);
 const Articles = () => (
@@ -10,9 +11,9 @@ const Articles = () => (
     <div className={cx('container', 'container-medium')}>
       <div className={cx('articles-container')}>
         <h2 className={cx('title')}>Useful articles</h2>
-        <a href="#" className={cx('link')}>
+        <Link to="#" className={cx('link')}>
           View all articles
-        </a>
+        </Link>
       </div>
 
       <ul className={cx('list')}>
@@ -28,16 +29,16 @@ const Articles = () => (
 
           <div className={cx('content-info')}>
             <p className={cx('info-time')}>
-              <time datetime="2022-07-03"> Jule 03, 2022</time>
+              <time dateTime="2022-07-03"> Jule 03, 2022</time>
             </p>
             <h3 className={cx('info-title')}> Discover Architecture</h3>
             <p className={cx('info-text')}>
               Projects for many large domestic and foreign corporations, enterprises in many elds
               such
             </p>
-            <a href="#" className={cx('info-link')}>
+            <Link to="#" className={cx('info-link')}>
               Learn more
-            </a>
+            </Link>
           </div>
         </li>
 
@@ -53,16 +54,16 @@ const Articles = () => (
 
           <div className={cx('content-info')}>
             <p className={cx('info-time')}>
-              <time datetime="2022-07-03"> Jule 03, 2022</time>
+              <time dateTime="2022-07-03"> Jule 03, 2022</time>
             </p>
             <h3 className={cx('info-title')}> Discover Architecture</h3>
             <p className={cx('info-text')}>
               Projects for many large domestic and foreign corporations, enterprises in many elds
               such
             </p>
-            <a href="#" className={cx('info-link')}>
+            <Link to="#" className={cx('info-link')}>
               Learn more
-            </a>
+            </Link>
           </div>
         </li>
 
@@ -78,16 +79,16 @@ const Articles = () => (
 
           <div className={cx('content-info')}>
             <p className={cx('info-time')}>
-              <time datetime="2022-07-03"> Jule 03, 2022</time>
+              <time dateTime="2022-07-03"> Jule 03, 2022</time>
             </p>
             <h3 className={cx('info-title')}> Discover Architecture</h3>
             <p className={cx('info-text')}>
               Projects for many large domestic and foreign corporations, enterprises in many elds
               such
             </p>
-            <a href="#" className={cx('info-link')}>
+            <Link to="#" className={cx('info-link')}>
               Learn more
-            </a>
+            </Link>
           </div>
         </li>
       </ul>
