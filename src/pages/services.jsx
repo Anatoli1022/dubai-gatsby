f1cc8e9fd27109db59d84107ca7f0e212a852c48
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-11-14 01:15:10 +0300

    feat: adds component hero in page services

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
+
+import Information from 'components/pages/services/information';
+import Challenge from 'components/pages/services/challenge';
 import Contact from 'components/shared/contact';
 import Propose from 'components/pages/services/propose';
-
+import Hero from 'components/pages/services/hero';
 const Services = () => (
   <Layout>
+    <Hero />
+    <Information />
+    <Challenge />
     <Contact />
     <Propose />
   </Layout>
