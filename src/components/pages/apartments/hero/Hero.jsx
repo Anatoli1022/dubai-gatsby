ff2f370d5e20ed013f6f068ac1c07419eb58cfa5
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-11-15 01:02:23 +0300

    feat: adds section hero in page apartments

---
+++ src/components/pages/apartments/hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Hero.module.scss';
+import classNames from 'classNames/bind';
+
+const cx = classNames.bind(styles);
+import { StaticImage } from 'gatsby-plugin-image';
+const Hero = () => {
+  return (
+    <section>
+      <div className={cx('container', 'container-large', 'hero-container')}>
+        <div className={cx('wrapper-text')}>
+          <h1 className={cx('title')}>
+            Apartments <span className={cx('title-span')}>in Dubai</span>
+          </h1>
+        </div>
+        <StaticImage
+          src="../../../../images/background.jpg"
+          className={cx('background')}
+          loading="eager"
+          aria-hidden="true"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
