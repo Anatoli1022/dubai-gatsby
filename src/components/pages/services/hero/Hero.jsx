39c14b76214aedaf419b76cdd599f9c425955a92
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-11-15 20:05:29 +0300

    fix: fix section hero in services

--- src/components/pages/services/hero/Hero.jsx
+++ src/components/pages/services/hero/Hero.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import styles from './Hero.module.scss';
 import classNames from 'classNames/bind';
-import background from './images/background.jpg';
+
 const cx = classNames.bind(styles);
+import { StaticImage } from 'gatsby-plugin-image';
 const Hero = () => {
   return (
     <section>
       <div className={cx('container', 'container-large', 'hero-container')}>
-        <span className={cx('hero-text')}>Rent</span>
-        <h1 className={cx('title')}>
-          Furnished Apartments <span className={cx('title-span')}>in Dubai</span>
-        </h1>
-        <img src={background} className={cx('background')} />
+        <div className={cx('wrapper-text')}>
+          <span className={cx('hero-text')}>Rent</span>
+          <h1 className={cx('title')}>
+            Furnished Apartments <span className={cx('title-span')}>in Dubai</span>
+          </h1>
+        </div>
+        <StaticImage
+          src="./images/background.jpg"
+          className={cx('background')}
+          loading="eager"
+          aria-hidden="true"
+        />
       </div>
     </section>
   );
